9690c293cfbab8df96702f3002ff8cf125ea2dc5
rommomm <rommomm@i.ua>
2024-04-17 22:15:18 +0300

    2

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
-console.log('start')
+import 'dotenv/config'
+import { Bot, InlineKeyboard, Keyboard } from 'grammy'
+
+const addresses = [
+	'Address 1',
+	'Address 2',
+	'Address 3',
+	'Address 4',
+	'Address 5',
+	'Address 6',
+]
+const addresses1 = [
+	{ address: 'Random', personalAccount: 4343443342, electricityMeterType: 1 },
+	{
+		address: 'Random123',
+		personalAccount: '777453443342',
+		electricityMeterType: '3',
+	},
+]
+
+const bot = new Bot(process.env.TG_TOKEN)
+
+bot.api.setMyCommands([
+	{
+		command: 'add_adresses',
+		description: 'Додати нову адресу',
+	},
+	{
+		command: 'get_adresses',
+		description: 'Отримати свої адреси',
+	},
+])
+
+bot.command('add_adresses', async ctx => {
+	await ctx.reply('121221')
+})
+
+bot.command('get_adresses', async ctx => {
+	const inlineKeyboard = new InlineKeyboard()
+	addresses1.forEach((a, i) => {
+		inlineKeyboard.text(a.address, a.personalAccount)
+	})
+
+	await ctx.reply('Оберіть адресу для передачі показань', {
+		reply_markup: inlineKeyboard,
+	})
+})
+
+bot.on('callback_query:data', async ctx => {
+	console.log(ctx)
+	await ctx.reply('wewe')
+})
+
+bot.hears(
+	addresses1.map(a => a.address),
+	async ctx => {
+		console.log(ctx.message.text)
+		await ctx.reply('Вкажіть ваші показння', {
+			reply_markup: { remove_keyboard: true },
+		})
+	}
+)
+
+bot.start()
