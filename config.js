b7760c828240a82f0a8f557de0cc07727e08eb09
rommomm <rommomm@i.ua>
2024-04-19 09:01:48 +0300

    2

--- config.js
+++ config.js
@@ -69,9 +69,9 @@ export async function sendDataToServer(data) {
 					'User-Agent':
 						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
 				},
-				// httpsAgent: new https.Agent({
-				// 	rejectUnauthorized: false,
-				// }),
+				httpsAgent: new https.Agent({
+					rejectUnauthorized: false,
+				}),
 			}
 		)
 
